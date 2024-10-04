1b738a610d552e15438d99f83128167cb83d9c7f
Priyanshu <priyanshu88253454@gmail.com>
2024-10-04 17:03:08 +0530

    Added lec1

---
+++ lec1/code.py
@@ -0,0 +1,11 @@
+# import code # This will create .pyc file
+# print("Hello World!", 2024, sep="-", end=" ")
+print("Hii")
+
+# This is
+# a multiline
+# comment
+
+data = input("Enter your name: ")
+
+print(data)
---
+++ lec1/sol.py
@@ -0,0 +1,23 @@
+# Question 1
+# name = input("Enter your name: ")
+# print("Hello, " + name + "! Welcome to Python class.")
+
+# Question 2
+# color = input("What is your favorite color? ")
+# print(color+" is a beautiful color!")
+
+# Question 3
+# name = input("Enter your name: ")
+# print(name, name, name, sep="\n")
+# print(name)
+# print(name)
+
+# Question 4
+# name = input("What is your name: ")
+# place = input("Where u from: ")
+# print("Nice to meet u, "+ name)
+# print("I have heard "+place+" is a great place.")
+
+# Question 5
+day = input("How was your day: ")
+print("Glad to hear that your day was "+day+".")
